defdeb8294ad7f468ef4fa1b68a191319807c1f3
rayanvpp <klsvppcs2023rayanhundekar@gmail.com>
2025-04-26 09:44:18 +0530

    Exam.java

--- exam.java
+++ exam.java
@@ -2,5 +2,5 @@ class introduction {
 
     public static void main(String[] args)
     {
-       System.out.println("HI I Am Rayan giving my 4th Sem End Exam")
+       System.out.println("HI I Am Rayan giving my 4th Sem End Exam and my topic is HOSPITAL MANAGEMENT")
     }
